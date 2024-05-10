38295c6d34abf075e9376e59da210bcfffcbb5cc
walk-on-me <rabifromthenight@gmail.com>
2024-05-10 19:59:25 +0200

    Add the build

--- build/index.js
+++ build/index.js
@@ -23,7 +23,7 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.uniswapLp = exports.curveLpMapping = exports.curveLp = exports.thiefConfig = exports.stakeDaoERC20 = exports.convexERC20 = exports.commonERC20 = exports.routers = exports.stakeDaoContracts = exports.convexContracts = exports.chainlinkPriceFeed = void 0;
+exports.durations = exports.curveLpMapping = exports.stakeDaoMapping = exports.balancerLp = exports.uniswapLp = exports.curveLp = exports.thiefConfig = exports.stakeDaoERC20 = exports.convexERC20 = exports.commonERC20 = exports.routers = exports.stakeDaoContracts = exports.convexContracts = exports.chainlinkPriceFeed = void 0;
 // Contracts
 exports.chainlinkPriceFeed = __importStar(require("./ressources/contracts/chainlinkPriceFeeds"));
 exports.convexContracts = __importStar(require("./ressources/contracts/convex"));
@@ -36,5 +36,10 @@ exports.stakeDaoERC20 = __importStar(require("./ressources/erc20/stakeDao"));
 exports.thiefConfig = __importStar(require("./ressources/erc20/thiefConfig"));
 // LP
 exports.curveLp = __importStar(require("./ressources/lps/curve"));
-exports.curveLpMapping = __importStar(require("./ressources/lps/curveLpMapping"));
 exports.uniswapLp = __importStar(require("./ressources/lps/uniswap"));
+exports.balancerLp = __importStar(require("./ressources/lps/balancer"));
+// MAPPINGS
+exports.stakeDaoMapping = __importStar(require("./ressources/mappings/stakeDao"));
+exports.curveLpMapping = __importStar(require("./ressources/mappings/curveLp"));
+// UTILS
+exports.durations = __importStar(require("./utils/durations"));
