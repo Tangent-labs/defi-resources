fcbaca7ca26101da73d8b5f4f04082a23e85a56d
yaje <yann.j@zohomail.com>
2025-07-07 18:32:48 +0200

    Add pendle router

--- build/ressources/contracts/routers.js
+++ build/ressources/contracts/routers.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.CURVE_V1_2_ROUTER = exports.UNISWAP_V3_ROUTER = exports.FRAXSWAP_ROUTER = void 0;
+exports.PENDLE_ROUTER_V4 = exports.CURVE_V1_2_ROUTER = exports.UNISWAP_V3_ROUTER = exports.FRAXSWAP_ROUTER = void 0;
 exports.FRAXSWAP_ROUTER = "0xc14d550632db8592d1243edc8b95b0ad06703867";
 exports.UNISWAP_V3_ROUTER = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
 exports.CURVE_V1_2_ROUTER = "0x45312ea0eFf7E09C83CBE249fa1d7598c4C8cd4e";
+exports.PENDLE_ROUTER_V4 = "0x888888888889758F76e7103c6CbF23ABbF58F946";
