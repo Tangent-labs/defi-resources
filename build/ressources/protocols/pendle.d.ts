59c6975d75870fb86e9049340edbeede69326591
walk-on-me <rabifromthenight@gmail.com>
2025-07-16 11:29:15 +0200

    Add some configuration in Pendle

--- build/ressources/protocols/pendle.d.ts
+++ build/ressources/protocols/pendle.d.ts
@@ -1,18 +1,8 @@
 export declare const PendlePools: {
-    "eUSDe 05/29/25": {
+    [key: string]: {
         MARKET: string;
         PT: string;
-    };
-    "eBTC 06/26/25": {
-        MARKET: string;
-        PT: string;
-    };
-    "sUSDe 07/31/25": {
-        MARKET: string;
-        PT: string;
-    };
-    "wstUSR 07/25/25": {
-        MARKET: string;
-        PT: string;
+        SY: string;
+        YT: string;
     };
 };
