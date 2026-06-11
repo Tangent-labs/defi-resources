b0369a555272fc60d736dd8398f9c5de8decd587
Vincent <vincent.granie@2018.icam.fr>
2026-06-11 22:52:58 +1200

    add morpho config

---
+++ build/ressources/protocols/morpho/markets.d.ts
@@ -0,0 +1,9 @@
+export declare const MORPHO_MARKETS: {
+    singleton: string;
+    "sUSG-frxUSD": {
+        id: string;
+        collateralToken: string;
+        loanToken: string;
+        creationBlock: number;
+    };
+};
---
+++ src/ressources/protocols/morpho/markets.ts
@@ -0,0 +1,9 @@
+export const MORPHO_MARKETS = {
+    singleton: "0xbbbbbbbbbb9cc5e90e3b3af64bdaf62c37eeffcb",
+    "sUSG-frxUSD": {
+        id: "0x2a2f62fe3d123077da35f281fbe69ebc296759b34873d627cf44c94f05fecf7e",
+        collateralToken: "0xf17d6f98a5c6eaa99d149079984119e0a4ef6900",
+        loanToken: "0xcacd6fd266af91b8aed52accc382b4e165586e29",
+        creationBlock: 25277170,
+    },
+};
